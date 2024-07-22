44f0da39bce0cf1e249b9fcf4633feb77ea1c6ce
Abdul-Moiz-Khan1 <moizz1326@gmail.com>
2024-07-22 15:48:12 +0500

    under dev

--- app/src/main/java/com/example/goodyoutubeshorts/Model/Video_Model.kt
+++
@@ -1,9 +0,0 @@
-package com.example.goodyoutubeshorts.Model
-
-data class Video_Model(
-    var videoId: String = "",
-    var tile: String = "",
-    var url: String = "",
-    var uploaderid: String = "",
-    var time: String = ""
-)
